83a8d05b83b99d801a759a4eebfed1f5c2075191
Talha Jahangiri Khan <tsjahangiri952@gmail.com>
2026-04-12 19:05:09 +0200

    fix: init

--- src/test/java/com/device/management/device_service/TestFactory.java
+++ src/test/java/com/device/management/device_service/TestFactory.java
@@ -2,13 +2,12 @@ package com.device.management.device_service;
 
 import com.device.management.device_service.domain.DeviceEntity;
 import com.device.management.device_service.domain.State;
+import com.device.management.device_service.dto.request.DevicePatchRequest;
 import com.device.management.device_service.dto.request.DeviceRequest;
 import com.device.management.device_service.dto.response.DeviceResponse;
 
 import java.util.List;
 import java.util.UUID;
-
-import com.device.management.device_service.dto.request.DevicePatchRequest;
 
 public final class TestFactory {
 
